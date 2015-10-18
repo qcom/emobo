596695e22068ad30fbdc1c8e62de18d2e37db3cc
Zach Hodge <qcom100@gmail.com>
2015-10-18 04:20:05 -0700

    swap the in-memory faux database for a redis persistence layer with a Store model interface

---
+++ src/lib/Store.js
@@ -0,0 +1,25 @@
+var Promise = require('bluebird');
+var redis = require('redis');
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+const PREFIX = 'emobo';
+let escapeKey = (key) => `${PREFIX}:${key}`;
+let dictionaryKey = escapeKey('dictionary');
+
+let client = redis.createClient();
+let Store = {};
+
+Store.get = (key) => client.hgetAsync(dictionaryKey, key);
+Store.getAll = () => client.hgetallAsync(dictionaryKey);
+Store.getDigest = () => {
+	return Store.getAll().then((emobos) => {
+		return Object.keys(emobos)
+			.map((key) => `${key}: ${emobos[key]}`)
+			.join('\n');
+	});
+};
+
+Store.set = (key, val) => client.hsetAsync(dictionaryKey, key, val);
+
+module.exports = Store;
--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ var config = require('config');
 
 /* internal dependencies */
 var lib = require('./lib');
+var Store = lib.Store;
+
 var middleware = require('./middleware');
 
 /* globals */
@@ -18,64 +20,61 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* data store */
-var emobos = {
-	'amigo': ':wave::skin-tone-4: :man::skin-tone-4: :flag-mx:',
-	'dreamteam': ':ryan-dreamteam: :pam-dreamteam: :vikram-dreamteam:',
-	// gang's all here
-	//'ensemble': 'fetch from web api'
-};
-
 // register a combination in the data store
 app.route('/combo')
 	.post(middleware.auth('combo'))
 	.post(function handleCombo(req, res, next) {
 		var {key, val, options} = lib.parse(req.body.text);
-		let definition;
-		// options.f = force write
-		if (definition = emobos[key] && !options.f) {
-			return res.end(`
-				nice try, trickster;
-				${key} is already defined: ${definition}
-			`);
-		}
-		emobos[key] = val;
-		res.end(`
-			emobo added:
-			${key} = ${val}
-		`);
+		Store.get(key).then(function(definition) {
+			// options.f = force write
+			if (definition && !options.f) {
+				return res.end(`
+					nice try, trickster;
+					${key} is already defined: ${definition}
+				`);
+			}
+			Store.set(key, val).then(function() {
+				res.end(`
+					emobo added:
+					${key} = ${val}
+				`);
+			});
+		});
 	});
 
 // expand the provided string with the stored dictionary
 app.route('/x')
 	.post(middleware.auth('x'))
 	.post(function handleX(req, res, next) {
-		let source = req.body.text;
-		let expanded = lib.expand(source, emobos);
+		Store.getAll().then(function(emobos) {
+			let source = req.body.text;
+			let expanded = lib.expand(source, emobos);
 
-		let channel = req.body.channel_id;
-		let payload = {
-			token: config.get('Slack.token'),
-			channel: channel,
-			text: expanded
-		};
+			let channel = req.body.channel_id;
+			let payload = {
+				token: config.get('Slack.token'),
+				channel: channel,
+				text: expanded
+			};
 
-		Slack.chat.postMessage(payload, function(err, data) {
-			if (err) return next(err);
-			if (!data.ok) {
-				return console.log(arguments);
-			}
+			Slack.chat.postMessage(payload, function(err, data) {
+				if (err) return next(err);
+				if (!data.ok) {
+					return console.log(arguments);
+				}
+			});
 		});
 	});
 
-// furnish and return a summary string of all stored emobos
+// furnish and return a digest of all stored emobos
 app.route('/emobos')
 	.post(middleware.auth('emobos'))
 	.post(function handleEmobos(req, res, next) {
-		let summary = Object.keys(emobos)
-			.map((key) => `${key}: ${emobos[key]}`)
-			.join('\n');
-		res.end(summary);
+		Store.getDigest().then(function(digest) {
+			res.end(digest);
+		});
+	});
+
 	});
 
 app.listen(8001);
