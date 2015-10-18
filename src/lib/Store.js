a1f78d9673e33104b1f10e93dfb462dbf4d763fd
Zach Hodge <qcom100@gmail.com>
2015-10-18 16:06:44 -0700

    clarify Store.js sections

--- src/lib/Store.js
+++ src/lib/Store.js
@@ -1,14 +1,17 @@
+/* user land dependencies */
 var Promise = require('bluebird');
 var redis = require('redis');
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
 
+/* globals */
 const PREFIX = 'emobo';
 let escapeKey = (key) => `${PREFIX}:${key}`;
 let dictionaryKey = escapeKey('dictionary');
-
 let client = redis.createClient();
-let Store = {};
+
+/* Store.js */
+let Store = module.exports = {};
 
 Store.get = (key) => client.hgetAsync(dictionaryKey, key);
 Store.getAll = () => client.hgetallAsync(dictionaryKey);
@@ -22,5 +25,3 @@ Store.getDigest = () => {
 
 Store.set = (key, val) => client.hsetAsync(dictionaryKey, key, val);
 Store.unset = (key) => client.hdelAsync(dictionaryKey, key);
-
-module.exports = Store;
