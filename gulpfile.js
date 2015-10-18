7b3855f1af0498e694a83660736ab6494e66601d
Zach Hodge <qcom100@gmail.com>
2015-10-18 02:55:07 -0700

    don't crash build process on syntax errors thanks to gulp plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 
@@ -8,9 +9,11 @@ var paths = {
 
 gulp.task('js', function() {
 	return gulp.src(paths.js)
+		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(sourcemaps.write('.'))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('dist'));
 });
 
